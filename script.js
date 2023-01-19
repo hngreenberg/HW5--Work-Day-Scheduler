1c38c9c8f66058bf5196c4b98c9b7668c924dc17
Heather Greenberg <hgreenberg1017@gmail.com>
2023-01-19 00:30:24 -0800

    add html and styling

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+$(document).ready(function () {
+  $("#currentDay").text(dayjs().format("dddd   MMMM D, YYYY"));
+});
+
+
+
+
+
+
 $(function () {
+  $(".saveBtn").on("click", function (){
+    console.log(this);
+    var text = $(this).siblings("description").val();
+    var time = $(this).parent().attr("id");
+
+    localStorage.setItem(time,text); }) 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
